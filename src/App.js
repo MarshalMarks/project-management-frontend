155e56234e662983ad2c776411954aa6203c54b6
Marshal Marks <marshmarks@gmail.com>
2023-10-27 19:38:31 -0500

    Changed page files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
-import { useState, useEffect } from "react";
+//import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import NoPage from './pages/NoPage';
+import ProjectDashboard from './pages/ProjectDashboard';
+import Profile from './pages/Profile';
 
 function App() {
-  const [user, setUser] = useState(null);
+  //const [user, setUser] = useState(null);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Login" element={<Login/>} />
-        <Route path="/Register" element={<Register/>} />
-        <Route path="/Dashboard" element={<Dashboard/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/user" element={<Profile/>} />
+        <Route path="/project" element={<ProjectDashboard/>} />
         <Route path="/*" element={<NoPage/>} />
       </Routes>
     </BrowserRouter>
