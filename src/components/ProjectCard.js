b9425239fa6a1c0756df4c9b680d3c63dce62621
Marshal Marks <marshmarks@gmail.com>
2023-11-20 17:23:19 -0600

    Configured Router to respond to user status

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,12 @@
+function ProjectCard() {
+    return (
+        <div className="projectCard">
+            <h1>Project Title</h1>
+            <p>Project description</p>
+        </div>
+    );
+}
+
+export {
+    ProjectCard
+};
